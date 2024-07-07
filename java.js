9777932a1769a37ed8bf14cc1ae4e1538b0e74de
Martin <martincasona085@gmail.com>
2024-07-06 22:41:06 -0500

    js

--- java.js
+++ java.js
@@ -4,16 +4,33 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+  const audio = document.getElementById("background-audio");
   document.addEventListener("DOMContentLoaded", () => {
     // Reproducir el audio y abrir la nueva página cuando el usuario haga clic en el div "carelly"
     document.querySelector(".carelly").addEventListener("click", async () => {
-            const audio = document.getElementById("background-audio");
-
-            await Promise.all([audio.play(), new Promise(resolve => {
-                resolve();
-            })])
-            .then(() => {
-                alerta=alert("Feliz 18 añoooos :) espero que te guste el regalo");
-            })
-    });
-});
+      audio.setTimeout=0;
+      audio.pause();
+                if (confirm("¿Sabes que dia es hoooy?")) {
+                  alert("eso es 'Aceptar', no es una respuesta como tal a mi pregunta, a ver de nuevo");
+                  alert("¿Sabes que dia es hoy?");
+                  alert("~(-_-~)");
+                  alert("Si, eso sigue siendo 'Aceptar'");
+                  alert("Bueno mira lo tomare como 'SI'");
+                  audio.play().then(() => {
+                    setTimeout(() => {
+                      alert("siii, tu cumpleañooooos, tus 18 años");
+                      alert("Felicidades Carelly, pasala bonito, te deseo lo mejor, que cumplas muchos años mas y que todos tus sueños se hagan realidad");
+                      alert("Me guardas torta :D");
+                    }, 100);
+                  });
+                }else{
+                    alert("Cancelar? en serio?");
+                    alert("mhhhm")
+                    alert("No pense que le darias a cancelar")
+                    alert("Segura que quieres cancelar?")
+                    alert("Bueno, si tu lo dices")
+                    window.location.href = "https://youtu.be/JhDIILjlhBQ?t=2";
+                }
+    }
+    );
+          });
