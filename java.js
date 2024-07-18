97aed8db38566553e139df9a6ccb43058385bbf3
Martin <martincasona085@gmail.com>
2024-07-18 16:59:50 -0500

    comiit2

--- java.js
+++ java.js
@@ -3,7 +3,14 @@ onload = () => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
     }, 1000);
-  };
+
+    const targetDate = new Date(new Date().getFullYear(), 7, 13);
+    const now = new Date();
+    const difference = targetDate - now;
+    if (difference > 0) {
+        window.location.href = "index.html";
+    }
+  }
   const audio = document.getElementById("background-audio");
   document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".carelly").addEventListener("click", async () => {
